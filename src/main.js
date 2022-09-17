68ddda97e4e1684f5fb213b6bf76ffe06372b44a
LeonLu3303 <leon.lu1103@gmail.com>
2022-09-17 21:04:10 +0800

    First commit 加入gasp跟ant套件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import { DatePicker } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(DatePicker).use(store).use(router).mount('#app');
