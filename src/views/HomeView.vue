01fd7c40da29da380050d6fad244d141773532d0
EstherChenGithub <maru@77yen.com>
2022-09-19 16:25:26 +0800

    no message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
-    <img alt="Vue logo" src="../assets/logo.png">
-=======
-    <img alt="Vue logo" src="../assets/images/main_logo_row.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
->>>>>>> 012a162a469fc023cbc8962045489a7f7b7549c9
+    <img alt="Vue logo" src="../assets/images/main_logo_row.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     HelloWorld,
-}
-}
+  },
+};
 </script>
