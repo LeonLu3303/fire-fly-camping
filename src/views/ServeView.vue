3f36edbbabad0d08befe6c0eaf81520d7c6b79c3
yishan <star138020@gmail.com>
2022-09-21 15:08:37 +0800

    營區服務

---
+++ src/views/ServeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <ServeTent/>
+    <ServeFood/>
+</template>
+
+<script>
+import ServeTent from '@/components/ServeTent.vue';
+import ServeFood from '@/components/ServeFood.vue';
+
+export default{
+    components: {
+        ServeTent,
+        ServeFood
+    },
+}
+</script>
+
+<style>
+
+</style>
