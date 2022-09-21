73f4be9967b737ef2ee6a85edfb9e571b5ddf636
yishan <star138020@gmail.com>
2022-09-21 13:40:57 +0800

    報告 修改

--- src/App.vue
+++ src/App.vue
@@ -7,30 +7,29 @@
     <router-link to="/booking">營區預訂</router-link>|
     <router-link to="/Shopping">Shopping</router-link>|
     <router-link to="/report">營火報告</router-link>|
-    <router-link to="/News">最新消息</router-link>|
-  </nav> -->
+  </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
-  // #app {
-  //   font-family: Avenir, Helvetica, Arial, sans-serif;
-  //   -webkit-font-smoothing: antialiased;
-  //   -moz-osx-font-smoothing: grayscale;
-  //   text-align: center;
-  //   color: #2c3e50;
-  // }
-  
-  // nav {
-  //   padding: 30px;
-  
-  //   a {
-  //     font-weight: bold;
-  //     color: #2c3e50;
-  
-  //     &.router-link-exact-active {
-  //       color: #42b983;
-  //     }
-  //   }
-  // }
-  </style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+}
+
+// nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+</style>
