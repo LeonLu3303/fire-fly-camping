f51feeff04891688eb538952ff6356f365575142
LeonLu3303 <leon.lu1103@gmail.com>
2022-09-23 08:59:05 +0800

    asdf

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-<<<<<<< HEAD
   transpileDependencies: true,
+  publicPath: '/cgd102/g1',
 });
-=======
-  transpileDependencies: true ,
-  publicPath:'/cgd102/g1'
-})
->>>>>>> 61f9f1c7303b11b1615882792c0da85f5b84e73d
