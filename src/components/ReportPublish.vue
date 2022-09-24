d69095bc2cd86ea4bb333dd392ac47a007b950e0
yishan <star138020@gmail.com>
2022-09-25 00:26:57 +0800

    新增 圖片 input btn

--- src/components/ReportPublish.vue
+++ src/components/ReportPublish.vue
@@ -141,7 +141,7 @@ export default {
         width: 100%;
         height: 50px;
         border-radius: 10px;
-        padding: 10px;
+        padding: 5px 10px;
         border: 1px solid $color-main-green;
         margin: 0 0 20px;
     }
