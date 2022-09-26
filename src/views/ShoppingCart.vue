886064af9cbba6ead19aba56ca29cfad56e70e36
EstherChenGithub <maru@77yen.com>
2022-09-26 20:39:46 +0800

    hi 你好

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -118,7 +118,7 @@ export default {
 
 </script>
 <style lang="scss" scoped>
-@import "../assets/scss/style.scss";
+@import "@/assets/scss/style.scss";
     .cart_section{
         background-color: $color-main-yellow;
         .cart_container {
