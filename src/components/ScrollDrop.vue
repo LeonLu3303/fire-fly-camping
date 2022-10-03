31d8a8953b44a9f25d62b1a88a85cac1d4ff3e20
EstherChenGithub <maru@77yen.com>
2022-10-03 19:29:18 +0800

    activity 頁面新增scroll down 提示

--- src/components/ScrollDrop.vue
+++ src/components/ScrollDrop.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="scroll_container">
-        <div class="scroll_text">scroll</div>
+        <div class="scroll_text"><p>scroll</p></div>
         <div class="scroll_group">
             <div class="scroll_line"></div>
             <div class="ball_drop"></div>
@@ -9,13 +9,14 @@
 </template>
 <script>
     import {gsap} from 'gsap'
+    
 export default {
     mounted() {
         const scroll = document.querySelector('.ball_drop')
 
     gsap.to( scroll, {
         autoAlpha: 0,
-        y: 110,
+        y: 120,
         // rotation: 90,
         ease: "power4",
         duration: 4,
@@ -32,20 +33,26 @@ export default {
         width: 100px;
         padding: 1rem;
     }
+    .scroll_text{
+        padding: 1rem 0;
+        text-align: center;
+        color: #0e685e;
+    }
     .scroll_line{
-        height: 118px;
+        height: 115px;
         width: 2px;
-        background-color: gray;
+        background-color: #fff;
         position: relative;
+        margin-left: 2rem;
     }
     .ball_drop{
         width: 20px;
         height: 20px;
         border-radius: 50%;
-        background-color: #000;
+        background-color: #0e685e;
         position: absolute;
-        left: 15px;
-        top: 40px;
+        left: 39px;
+        top: 45px;
         opacity: 1;
     }
 </style>
