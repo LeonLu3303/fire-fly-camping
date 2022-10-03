b0bfa89589277b0d1b0153c53edbd1ba9c748693
EstherChenGithub <maru@77yen.com>
2022-10-03 18:48:42 +0800

    +scroll drop animation

---
+++ src/components/ScrollDrop.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="scroll_container">
+        <div class="scroll_text">scroll</div>
+        <div class="scroll_group">
+            <div class="scroll_line"></div>
+            <div class="ball_drop"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    mounted() {
+        const scroll = document.querySelector('.ball_drop')
+
+    gsap.to( scroll, {
+        autoAlpha: 0,
+        y: 110,
+        // rotation: 90,
+        ease: "power4",
+        duration: 4,
+        stagger: 0.2,
+        repeatdelay: 0.3,
+        repeat: -1
+})
+    },
+    
+}
+</script>
+<style land="scss" scoped>
+    .scroll_container{
+        width: 100px;
+        padding: 1rem;
+    }
+    .scroll_line{
+        height: 118px;
+        width: 2px;
+        background-color: gray;
+        position: relative;
+    }
+    .ball_drop{
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-color: #000;
+        position: absolute;
+        left: 15px;
+        top: 40px;
+        opacity: 1;
+    }
+</style>
