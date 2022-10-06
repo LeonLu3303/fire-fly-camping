f984fa5d7b032aa95f3e33c15fbdb69de5e01aa9
yishan <star138020@gmail.com>
2022-10-06 15:59:59 +0800

    最新消息 修改

--- src/components/HomeNews.vue
+++ src/components/HomeNews.vue
@@ -119,7 +119,7 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 @import '../assets/scss/style.scss';
 @import '../assets/scss/component/HomeNews';
 </style>
